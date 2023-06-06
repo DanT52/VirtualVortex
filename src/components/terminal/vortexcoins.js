be9ad85db0eab331e8dde3e48af7aff23ee1bbb0
Daniel <dantramp52@gmail.com>
2023-06-05 21:22:52 -0700

    submins changed to 5-->1

--- src/components/terminal/vortexcoins.js
+++ src/components/terminal/vortexcoins.js
@@ -48,7 +48,7 @@ export async function searchVortexCoins(username, args){
         text = `You cannot search as:\n ${jailReason}\n you can search again in ${formatDistanceStrict(currentDate, inJail)}.`
     }
     else if (lastSearched > subMinutes(currentDate, 1)){
-        text = `You just searched...\n you are tired\n please wait ${formatDistanceStrict(lastSearched, subMinutes(currentDate, 5))}.`
+        text = `You just searched...\n you are tired\n please wait ${formatDistanceStrict(lastSearched, subMinutes(currentDate, 1))}.`
 
     }else{
         text = await longSearchResponse(username, args[0], data)
